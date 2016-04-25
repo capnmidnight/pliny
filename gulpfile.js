ee69d85ec664be7472b42a0e597dd6cdd59b2672
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-04-25 18:33:26 -0400

    task runner

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require("gulp"),
+  babel = require("gulp-babel"),
+  concat = require("gulp-concat"),
+  jshint = require("gulp-jshint"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify");
+
+gulp.task("lint", function () {
+  return gulp.src("index.js")
+    .pipe(jshint({
+      multistr: true
+    }));
+});
+
+gulp.task("default", ["lint"], function () {
+  return gulp.src(["node_modules/marked/marked.min.js", "index.js"])
+    .pipe(concat("pliny.js"))
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("./"));
+});
