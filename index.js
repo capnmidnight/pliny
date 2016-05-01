cfcfa205ab91190309cc62ec37000d17cb477325
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-04-30 23:18:12 -0400

    carve function

--- index.js
+++ index.js
@@ -815,6 +815,45 @@
   pliny.get = openBag.bind(null, pliny.database);
   // Forward on the markdown functionality
   pliny.markdown = marked;
+  // Strip pliny calls out of a source file and deposit them into a separate file.
+  pliny.carve = function (source, destination, callback) {
+      var fs = require("fs");
+      fs.readFile(source, "utf-8", function (err, txt) {
+        var matches,
+          left = 0,
+          outputLeft = "",
+          outputRight = "",
+          test = /pliny\.\w+/g;
+        while (matches = test.exec(txt)) {
+          var sub = txt.substring(left, matches.index);
+          outputLeft += sub;
+          var depth = 0, inString = false, found = false;
+          for (left = matches.index + matches.length; left < txt.length; ++left) {
+            if (txt[left] === "\"" && (left === 0 || txt[left - 1] !== "\\"))
+              inString = !inString;
+            if (!inString) {
+              if (txt[left] === "(") {
+                found = true;
+                ++depth;
+              }
+              else if (txt[left] === ")")
+                --depth;
+            }
+            if (depth === 0 && found)
+              break;
+          }
+          while (left < txt.length && /[;\) \r\n]/.test(txt[left])) {
+            left++;
+          }
+
+          outputRight += txt.substring(matches.index, left);
+        }
+        outputLeft += txt.substring(left);
+        fs.writeFile(source, outputLeft, function () {
+          fs.writeFile(destination, outputRight, callback);
+        });
+      });
+  };
 
   pliny.setEnumerationValues = setEnumerationValues;
 
