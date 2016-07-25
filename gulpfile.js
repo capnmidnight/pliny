ed25559d92d80c392ba89e94969ffb9208148307
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-07-25 11:59:22 -0400

    new build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-﻿require("notiontheory-basic-build")("pliny", ["node_modules/marked/marked.min.js", "src/index.js"]);
+﻿var gulp = require("gulp"),
+  nt = require("notiontheory-basic-build").setup(gulp),
+  tasks = nt.js(
+    "pliny",
+    ["src/**/*.js"],
+    ["node_modules/marked/lib/marked.js"]);
+
+gulp.task("default", [tasks.dev]);
+gulp.task("debug", [tasks.debug]);
+gulp.task("release", [tasks.release]);
